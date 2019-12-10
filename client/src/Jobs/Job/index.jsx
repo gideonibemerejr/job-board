90be124007b16ba6c50638b2222ae2826ecff603
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-12-10 00:32:54 -0600

    Update Jobs component with stepper

--- client/src/Jobs/Job/index.jsx
+++ client/src/Jobs/Job/index.jsx
@@ -5,12 +5,17 @@ const Job = ({ job }) => {
   return (
     <Paper className="job">
       <div>
-        <Typography>{job.title}</Typography>
-        <Typography>{job.company}</Typography>
+        <Typography variant="h6">{job.title}</Typography>
+        <Typography variant="h5">{job.company}</Typography>
         <Typography>{job.location}</Typography>
       </div>
       <div>
-        <Typography>{job.created_at}</Typography>
+        <Typography>
+          {job.created_at
+            .split(" ")
+            .slice(0, 3)
+            .join(" ")}
+        </Typography>
       </div>
     </Paper>
   );
--- client/src/Jobs/index.jsx
+++ client/src/Jobs/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
 import Job from "./Job";
+import { Typography, MobileStepper, Button } from "@material-ui/core";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
 const Jobs = ({ jobs }) => {
-  console.log("job is", jobs[0]);
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
 
   return (
     <>
@@ -14,6 +24,27 @@ const Jobs = ({ jobs }) => {
         {jobs.map((job, idx) => (
           <Job key={idx} job={job} />
         ))}
+        <MobileStepper
+          position="static"
+          variant="text"
+          activeStep={activeStep}
+          nextButton={
+            <Button size="small" onClick={handleNext}>
+              Next
+              <KeyboardArrowRight />
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              <KeyboardArrowLeft />
+              Back
+            </Button>
+          }
+        />
       </div>
     </>
   );
